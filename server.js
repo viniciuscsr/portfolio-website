5065ccb4aaf603d9dce58619e7317531ca7b328c
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2021-01-31 16:55:14 -0500

    add dotenv and change about image

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 5000;
 
 app.use('/public', express.static('public'));
 app.use(express.static(__dirname + '/public'));
