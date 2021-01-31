6c2a5ad11671664dca339679362e5e4028f138bb
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2021-01-31 14:46:07 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use('/public', express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('./index');
+});
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
